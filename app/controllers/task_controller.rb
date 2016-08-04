53d1f5b148290045a72d2596bf2c3b6b75d761f6
Calvin <Calvin.lacc@gmail.com>
2016-08-03 22:44:50 -0600

    Added the show function and sorted the task by date

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,6 +2,11 @@ class TaskController < ApplicationController
 
   def index
     @tasks = Task.all
+    @tasks = @tasks.sort_by { |task| task.date }
+  end
+
+  def show 
+    @tasks = Task.find(params[:id])
   end
 
 end
